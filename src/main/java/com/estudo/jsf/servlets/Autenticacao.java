6f2986b9148e114e95c5396a4e3e90d52240ae87
davis <davisonic2010@gmail.com>
2024-04-28 19:03:42 -0300

    First commit

--- src/main/java/com/estudo/jsf/servlets/Autenticacao.java
+++ src/main/java/com/estudo/jsf/servlets/Autenticacao.java
@@ -6,11 +6,12 @@ import jakarta.mail.Session;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebFilter("/autenticar")
+@WebFilter(urlPatterns = {"/home/*"})
 public class Autenticacao implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -28,16 +29,6 @@ public class Autenticacao implements Filter {
         String name = servletRequest.getParameter("nome");
         String password = servletRequest.getParameter("senha");
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        if (name == null || password == null) {
-
-            if (request.getServletPath().contains("logins")) {
-                request.setAttribute("message", "Você deve preencher cada um dos campos!!");
-                servletRequest.getRequestDispatcher("/login.jsp").forward(request, servletResponse);
-                return;
-            }
-            servletRequest.getRequestDispatcher("/index.jsp").forward(servletRequest, servletResponse);
-            return;
-        }
 
         SessionModel model = null;
         try {
@@ -45,10 +36,10 @@ public class Autenticacao implements Filter {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
         if (model == null) {
+
             if (request.getServletPath().contains("home")) {
-                servletRequest.getRequestDispatcher("/login.jsp").forward(request, servletResponse);
+                ((HttpServletResponse)servletResponse).sendRedirect("/logins/login.jsp");
                 return;
             }
         }
